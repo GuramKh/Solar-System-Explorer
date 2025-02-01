09d1b861477a339800bc93a8fd1412c18c85e6c2
GuramKh <guram.khakhutashvili@gmail.com>
2025-02-01 16:35:43 +0400

    Changed planets.ts the way it was first time

--- src/services/api/planets.ts
+++ src/services/api/planets.ts
@@ -1,27 +1,16 @@
-const BASE_URL = 'https://planets-api-yakh.onrender.com';
+const BASE_URL = '/api';
 
 const getPlanets = async () => {
-  const response = await fetch(`${BASE_URL}/planets`, {
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
-    }
-  });
+  const response = await fetch(`${BASE_URL}/planets`);
   const data = await response.json();
   return data;
 };
 
 const getPlanetByName = async (name: string) => {
-  const response = await fetch(`${BASE_URL}/planets/${name.toLowerCase()}`, {
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
-    }
-  });
+  const response = await fetch(`${BASE_URL}/planets/${name.toLowerCase()}`);
   const data = await response.json();
   return data;
 };
-
 
 export const planetsApi = {
   getPlanets,
