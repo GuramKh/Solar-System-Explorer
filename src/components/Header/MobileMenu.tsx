dab2c1ccd7df97491e4220f3ca744823fb1b840e
GuramKh <guram.khakhutashvili@gmail.com>
2025-02-01 16:51:26 +0400

    Made changes to MobileMenu.tsx

--- src/components/Header/MobileMenu.tsx
+++ src/components/Header/MobileMenu.tsx
@@ -3,7 +3,7 @@ import { NavLinks } from './NavLinks';
 export const MobileMenu = () => {
   return (
     <div className="md:hidden">
-      <div className="pt-2 pb-3 space-y-1">
+      <div className="pt-2 pb-3 space-x-4 flex flex-wrap gap-4">
         <NavLinks />
       </div>
     </div>
