07719323ced27539e92273c90b77e18b2ce3c09d
GuramKh <guram.khakhutashvili@gmail.com>
2025-02-01 16:25:51 +0400

    Made changes to planets.ts

--- src/services/api/planets.ts
+++ src/services/api/planets.ts
@@ -1,16 +1,27 @@
 const BASE_URL = 'https://planets-api-yakh.onrender.com';
 
 const getPlanets = async () => {
-  const response = await fetch(`${BASE_URL}/planets`);
+  const response = await fetch(`${BASE_URL}/planets`, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    }
+  });
   const data = await response.json();
   return data;
 };
 
 const getPlanetByName = async (name: string) => {
-  const response = await fetch(`${BASE_URL}/planets/${name.toLowerCase()}`);
+  const response = await fetch(`${BASE_URL}/planets/${name.toLowerCase()}`, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    }
+  });
   const data = await response.json();
   return data;
 };
+
 
 export const planetsApi = {
   getPlanets,
